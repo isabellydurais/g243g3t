96071a2e79c6886b87de5ec5a94d7d5e178bede6
isabellydurais <108758458+isabellydurais@users.noreply.github.com>
2024-10-23 08:11:54 -0300

    Add files via upload

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,19 +6,23 @@ async function buscarInformacoes(){
     const paragrafo = document.createElement('p');
     paragrafo.classList.add('graficos-container_texto');
 
-    const pessoasConectadas = dados.total-pessoas-conectadas / 1e9;
-    constpessoasNoMundo = dados.total-pessoas-mundo / 1e9;
+    const pessoasConectadas = dados.total_pessoas_conectadas / 1e9;
+    const pessoasNoMundo = dados.total_pessoas_mundo / 1e9;
     const horas = parseInt(dados.tempo_medio);
-    const minutos = Math.round((dados.tempo_medio - horas)*60);
-    const porcentagemConectada = ((pessoasConectadas/pessoasNoMundo)*100).toFixed(2);
-
+    const minutos = Math.round(dados.tempo_medio - horas)*60;
+    const porcentagemConectada = (pessoasConectadas/pessoasNoMundo)*100;
     paragrafo.innerHTML = `Você sabia que o mundo tem 
-    <span>${pessoasNoMundo}</span> de pessoas e
-    que aproximadamente <span>${pessoasConectadas} bilhões</span>
-    estão conectadas em alguma rede social e passam em média
-    <span>${horas}</span> horas e <span>${minutos}</span> minutos conectadas.
-    <br> Isso significa que aproximadamente <span>${porcentagemConectada}%</span>
-    de pessoas estão conectadas em alguma rede social.`;
+    <span>${pessoasNoMundo}</span> 
+    bilhões de pessoas e que aproximadamente 
+    <span>${pessoasConectadas}</span> 
+    bilhões estão conectadas em alguma rede social e passam em média 
+    <span>${horas}</span> 
+    horas e 
+    <span>${minutos}</span>
+    minutos conectadas.
+    Isso significa que aproximadamente 
+    <span>${porcentagemConectada}%</span>
+    de pessoas que estão conectadas em alguma rede social. `
     document.getElementById('graficos-container').appendChild(paragrafo);
 }
 
